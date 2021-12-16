593eb471212b36d7d51e74f5967256a3c5ad2762
Gustavo Conde <guoconde@pop-os.localdomain>
2021-12-15 23:56:01 -0300

    Implementando Assentos Conteudo/Style

--- src/Componentes/Assento/index.jsx
+++ src/Componentes/Assento/index.jsx
@@ -1,3 +1,49 @@
+import './style.css'
+
+import Rodape from '../Genericos/Rodape'
+
 export default function Assentos() {
-    return <h1>Assentos</h1>
+    return (
+        <>
+            <main className='main-assentos'>
+                <div className="titulo-pagina">Selecione o(s) assento(s)</div>
+                <div className="todos-assentos">
+                    <div className="assentos">
+                        <div className="assento">1</div>
+                        <div className="assento">2</div>
+                        <div className="assento">3</div>
+                        <div className="assento">4</div>
+                        <div className="assento">5</div>
+                        <div className="assento">6</div>
+                        <div className="assento">7</div>
+                        <div className="assento">8</div>
+                        <div className="assento">9</div>
+                        <div className="assento">10</div>
+                    </div>
+                    <div className="tipos-de-assentos">
+                        <div>
+                            <div className="assento selecionado"></div>
+                            Selecionado
+                        </div>
+                        <div>
+                            <div className="assento disponivel"></div>
+                            Disponível
+                        </div>
+                        <div>
+                            <div className="assento indisponivel"></div>
+                            Indisponível
+                        </div>
+                    </div>
+                </div>
+                <div className="dados">
+                    <p className='titulo-dados'>Nome do comprador:</p>
+                    <input type="text" placeholder='Digite seu nome...' />
+                    <p className='titulo-dados'>CPF do comprador:</p>
+                    <input type="text" placeholder='Digite seu CPF...' />
+                </div>
+                <button className='btn-reservar'>Reservar Assento(s)</button>
+            </main>
+            <Rodape />
+        </>
+    )
 }
