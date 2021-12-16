5559d8a1199892a08c0ad75b242186f945b31c98
Gustavo Conde <guoconde@pop-os.localdomain>
2021-12-15 22:50:03 -0300

    Implementacao Sessao Conteudo/Style

--- src/Componentes/Inicial/index.jsx
+++ src/Componentes/Inicial/index.jsx
@@ -1,23 +1,31 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
 
-import Topo from "../Genericos/Topo"
 import './style.css'
+import { Link } from "react-router-dom"
 
 export default function Inicial() {
     const [filmes, setFilmes] = useState([])
 
     useEffect(() => {
         const promessa = axios.get('https://mock-api.driven.com.br/api/v4/cineflex/movies')
-        promessa.then(filme => 
+        promessa.then(filme =>
             setFilmes(filme.data)
-            )
+        )
     }, [])
-    
+
+    const mapeando = filmes.map(filme =>
+        <Link to='/sessoes/:idFilme' id={filme.id} className="poster" >
+            <img src={filme.posterURL} key={filme.id} alt={filme.nome} />
+        </Link>
+        )
+
     return (
         <>
             <div className="titulo-pagina">Selecione o filme</div>
-            {filmes.map(filme => <img src={filme.posterURL} key={filme.id} />)}
+            <div className="filmes-em-cartaz">
+                {mapeando}
+            </div>
         </>
     )
 }
--- src/Componentes/Sessao/index.jsx
+++ src/Componentes/Sessao/index.jsx
@@ -1,3 +1,21 @@
+import './style.css'
+
 export default function Sessoes() {
-    return <h1>Sessoes</h1>
+    return (
+        <>
+            <div className="titulo-pagina">Selecione o horário</div>
+            <div className="sessoes">
+                <main className="conteudo-sessoes">
+                    <p className="data">Quinta-feira - 24/06/2021</p>
+                    <button className="btn-horario">15:00</button>
+                    <button className="btn-horario">19:00</button>
+                </main>
+                <main className="conteudo-sessoes">
+                    <p className="data">Sexta-feira - 25/06/2021</p>
+                    <button className="btn-horario">15:00</button>
+                    <button className="btn-horario">19:00</button>
+                </main>
+            </div>
+        </>
+    )
 }
